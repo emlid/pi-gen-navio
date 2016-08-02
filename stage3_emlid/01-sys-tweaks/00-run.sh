96b5d6f713234c591928b08a5e64e51190ab9275
Staroselskii Georgii <georgii.staroselskii@emlid.com>
2016-08-02 16:19:24 +0300

    stage3: added wpa_supplicant to local files

--- stage3_emlid/01-sys-tweaks/00-run.sh
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-install -m 644 ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf ${ROOTFS_DIR}/boot
+install -m 644 files/wpa_supplicant.conf ${ROOTFS_DIR}/boot
 ln -sf /boot/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
