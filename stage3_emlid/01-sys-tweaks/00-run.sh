9c944314dca4bec5709f67baa91d74dbf069d61d
Staroselskii Georgii <georgii.staroselskii@emlid.com>
2016-08-02 16:48:08 +0300

    stage3: added i2c-dev to modules

--- stage3_emlid/01-sys-tweaks/00-run.sh
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
 install -m 644 files/wpa_supplicant.conf ${ROOTFS_DIR}/boot
+install -m 644 files/modules ${ROOTFS_DIR}/etc/modules
+
 ln -sf /boot/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
