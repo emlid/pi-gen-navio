39a6fd56d1fa5391eec285952cc91b392315c525
Staroselskii Georgii <georgii.staroselskii@emlid.com>
2016-08-02 17:22:51 +0300

    stage3: disable bluetooth as it's not used

--- stage3_emlid/01-sys-tweaks/00-run.sh
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -4,3 +4,7 @@ install -m 644 files/wpa_supplicant.conf ${ROOTFS_DIR}/boot
 install -m 644 files/modules ${ROOTFS_DIR}/etc/modules
 
 ln -sf /boot/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
+
+on_chroot sh -e - <<EOF
+systemctl disable hciuart
+EOF
