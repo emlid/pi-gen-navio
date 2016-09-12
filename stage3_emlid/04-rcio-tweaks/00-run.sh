01922e97db68d0e68513740087f67d2750456445
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2016-09-12 18:10:17 +0300

    stage3: trying out

--- stage3_emlid/04-rcio-tweaks/00-run.sh
+++ stage3_emlid/04-rcio-tweaks/00-run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -ex
 
 ln -sf /usr/bin/dtc ${ROOTFS_DIR}/usr/local/bin/dtc
+
+mkdir -p ${ROOTFS_DIR}/files
+install -m 644 files/*.deb ${ROOTFS_DIR}/files
+
+on_chroot bash
+on_chroot dpkg -i ${ROOTFS_DIR}/files/rcio-firmware_0.1_armhf.deb ${ROOTFS_DIR}/files/rtl8812au-dkms_4.3.8.12175.20140902-0raspbian3_all.deb
