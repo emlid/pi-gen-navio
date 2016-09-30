8c66fae5b9ab58c74c66ae65b39ce198c30e1afa
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2016-09-30 17:35:33 +0300

    stage3: ardupilot: add mavproxy

--- stage3_emlid/05-ardupilot/00-run.sh
+++ stage3_emlid/05-ardupilot/00-run.sh
@@ -6,3 +6,7 @@ on_chroot ln -fs pkg-config /usr/bin/arm-linux-gnueabihf-pkg-config
 cp -r files ${ROOTFS_DIR}
 on_chroot dpkg -i files/*.deb
 rm -rf ${ROOTFS_DIR}/files
+
+on_chroot bash - << EOF
+    pip install mavproxy
+EOF
