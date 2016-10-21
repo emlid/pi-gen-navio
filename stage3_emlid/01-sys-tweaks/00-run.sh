f96dc9716463cc809e5d8a34c03e151cf9599031
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2016-10-21 20:09:24 +0300

    stage3: sys-tweaks: use en_US.UTF-8 locale set in /etc/environment

--- stage3_emlid/01-sys-tweaks/00-run.sh
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -4,6 +4,7 @@ install -m 644 files/wpa_supplicant.conf ${ROOTFS_DIR}/boot
 install -m 644 files/modules ${ROOTFS_DIR}/etc/modules
 install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot
 install -m 644 files/motd ${ROOTFS_DIR}/etc
+install -m 644 files/environment ${ROOTFS_DIR}/etc
 
 ln -sf /boot/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
 
