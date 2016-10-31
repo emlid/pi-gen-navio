3882e1ce6c4a180466396ac31b19a8dce29b868c
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2016-10-31 17:23:49 +0300

    stage3: ros: added force to symlink liblog4cxx

--- stage3_emlid/07-ros/02-run.sh
+++ stage3_emlid/07-ros/02-run.sh
@@ -5,6 +5,6 @@ cp files/libmavconn.so ${ROOTFS_DIR}/opt/ros/indigo/lib/libmavconn.so.18.3
 on_chroot sh - << EOF
 cp /opt/ros/indigo/lib/libmavconn.so /opt/ros/indigo/lib/libmavconn.so.17.4 
 ln -sf /opt/ros/indigo/lib/libmavconn.so.18.3 /opt/ros/indigo/lib/libmavconn.so 
-ln -s /usr/lib/arm-linux-gnueabihf/liblog4cxx.so /usr/lib/liblog4cxx.so
+ln -sf /usr/lib/arm-linux-gnueabihf/liblog4cxx.so /usr/lib/liblog4cxx.so
 EOF
 
