9fa1de80c2946653f2bde05b1184225caf1cdcf3
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2016-10-31 15:54:05 +0300

    stage3: apt-tweaks: added new GPG2 key

--- stage3_emlid/02-apt-tweaks/00-run.sh
+++ stage3_emlid/02-apt-tweaks/00-run.sh
@@ -3,6 +3,7 @@
 install -m 644 files/emlid.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
 
 on_chroot apt-key add - < files/emlid.gpg.key
+on_chroot apt-key add - < files/emlid-builder.gpg.key
 on_chroot sh -e - << EOF
 apt-get update
 apt-get dist-upgrade -y
