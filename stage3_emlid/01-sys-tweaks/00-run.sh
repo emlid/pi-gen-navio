9d18e3b4b08de555c33bba6aa6b524a54812cfbd
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2016-12-26 19:09:41 +0300

    stage3: enable ssh by default

--- stage3_emlid/01-sys-tweaks/00-run.sh
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -5,6 +5,7 @@ install -m 644 files/modules ${ROOTFS_DIR}/etc/modules
 install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot
 install -m 644 files/motd ${ROOTFS_DIR}/etc
 install -m 644 files/environment ${ROOTFS_DIR}/etc
+install -m 644 /dev/null ${ROOTFS_DIR}/ssh
 
 ln -sf /boot/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
 
