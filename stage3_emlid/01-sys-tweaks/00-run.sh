7506a91c523e974ee7c6f329e1468c0a2053237d
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2016-12-27 19:02:26 +0300

    stage3: sys-tweaks: use custom interfaces

--- stage3_emlid/01-sys-tweaks/00-run.sh
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -6,6 +6,7 @@ install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot
 install -m 644 files/motd ${ROOTFS_DIR}/etc
 install -m 644 files/environment ${ROOTFS_DIR}/etc
 install -m 644 /dev/null ${ROOTFS_DIR}/boot/ssh
+install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network
 
 ln -sf /boot/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
 
