8d5788a8b9fbda1070b7fd4d2c822d9234eef62b
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-01-16 16:19:05 +0300

    stage3: rcio: move rcio to cdn repo

--- stage3_emlid/04-rcio-tweaks/02-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-mkdir -p ${ROOTFS_DIR}/files
-install -m 644 files/*.deb ${ROOTFS_DIR}/files
-on_chroot << EOF
-dpkg -i files/rcio-firmware_0.1_armhf.deb
-EOF
-rm -rf ${ROOTFS_DIR}/files
