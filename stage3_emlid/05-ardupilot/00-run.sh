f356476f3f2ff71f8bab80caead0358b84596154
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-01-18 19:55:25 +0300

    stage3: ardupilot: use repo instead of hardcoded debs

--- stage3_emlid/05-ardupilot/00-run.sh
+++ stage3_emlid/05-ardupilot/00-run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 #a hack for waf to use. update-alternatives might be better
-on_chroot ln -fs pkg-config /usr/bin/arm-linux-gnueabihf-pkg-config
-
-cp -r files ${ROOTFS_DIR}
-on_chroot << EOF
-    dpkg -i files/*.deb
-EOF
-rm -rf ${ROOTFS_DIR}/files
-
-on_chroot << EOF
-    pip install mavproxy
+on_chroot << EOF 
+ln -fs pkg-config /usr/bin/arm-linux-gnueabihf-pkg-config
 EOF
