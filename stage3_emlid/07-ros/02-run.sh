2dcc7d92e99b17c03f857eb57c2684c5c32463c8
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-03-03 16:55:30 +0300

    stage3: ros: put mavros on hold

--- stage3_emlid/07-ros/02-run.sh
+++ stage3_emlid/07-ros/02-run.sh
@@ -6,5 +6,6 @@ on_chroot << EOF
 cp /opt/ros/indigo/lib/libmavconn.so /opt/ros/indigo/lib/libmavconn.so.17.4 
 ln -sf /opt/ros/indigo/lib/libmavconn.so.18.3 /opt/ros/indigo/lib/libmavconn.so 
 ln -sf /usr/lib/arm-linux-gnueabihf/liblog4cxx.so /usr/lib/liblog4cxx.so
+apt-mark hold ros-indigo-mavros
 EOF
 
