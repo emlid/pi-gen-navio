4c3863fc68ee144f5747cd91159c0a5ef042f626
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-03-03 19:09:33 +0300

    stage3: python: emlid: use proper navio2 from pypi

--- stage3_emlid/09-python/01-run.sh
+++ stage3_emlid/09-python/01-run.sh
@@ -7,7 +7,6 @@ curl -H "Authorization: token $AUTH_TOKEN" -L https://api.github.com/repos/emlid
 tar -xvf files/emlid.tar.gz -C ${ROOTFS_DIR}/home/pi/
 
 on_chroot << EOF
-pip3 install -i https://testpypi.python.org/pypi navio2
 pip3 install -r /home/pi/requirements3.txt
 pip2 install -r /home/pi/requirements2.txt
 pushd /home/pi/emlid*
