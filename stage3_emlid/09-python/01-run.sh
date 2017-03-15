11f1aae34ed7b630e8f7cc4d0694bb49ce88fa9d
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-03-15 16:16:18 +0300

    stage3: python: emlitool: remove old istallation method leftovers

--- stage3_emlid/09-python/01-run.sh
+++ stage3_emlid/09-python/01-run.sh
@@ -3,9 +3,6 @@
 cp -f files/requirements3.txt ${ROOTFS_DIR}/home/pi
 cp -f files/requirements2.txt ${ROOTFS_DIR}/home/pi
 
-curl -H "Authorization: token $AUTH_TOKEN" -L https://api.github.com/repos/emlid/emlid-autopilot-utils/tarball/v0.2 > files/emlid.tar.gz
-tar -xvf files/emlid.tar.gz -C ${ROOTFS_DIR}/home/pi/
-
 on_chroot << EOF
 pip3 install -r /home/pi/requirements3.txt
 pip2 install -r /home/pi/requirements2.txt
