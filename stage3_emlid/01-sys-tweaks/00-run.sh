a3f30fe9c4d8c12668ffa78bd19d7a1525dafd9b
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-03-20 16:24:03 +0300

    stage3: sys-tweaks: do not ship motd

--- stage3_emlid/01-sys-tweaks/00-run.sh
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -3,7 +3,6 @@
 install -m 644 files/wpa_supplicant.conf ${ROOTFS_DIR}/boot
 install -m 644 files/modules ${ROOTFS_DIR}/etc/modules
 install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot
-install -m 644 files/motd ${ROOTFS_DIR}/etc
 install -m 644 files/environment ${ROOTFS_DIR}/etc
 install -m 644 /dev/null ${ROOTFS_DIR}/boot/ssh
 install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network
