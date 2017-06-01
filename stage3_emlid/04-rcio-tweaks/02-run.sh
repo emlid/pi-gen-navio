62e48797ba21342560c94edf59c75feb0e7c0e4e
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-06-01 18:07:04 +0300

    stage3: rcio-tweaks: updated rcio-firmware to 1.5.4

--- stage3_emlid/04-rcio-tweaks/02-run.sh
+++ stage3_emlid/04-rcio-tweaks/02-run.sh
@@ -3,6 +3,6 @@
 mkdir -p ${ROOTFS_DIR}/files
 install -m 644 files/*.deb ${ROOTFS_DIR}/files
 on_chroot << EOF
-dpkg -i files/rcio-firmware_0.1_armhf.deb
+dpkg -i files/*.deb
 EOF
 rm -rf ${ROOTFS_DIR}/files
