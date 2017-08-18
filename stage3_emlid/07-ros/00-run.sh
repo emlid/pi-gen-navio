47f9293c648f9cdc578428472d486ea3304fe9bd
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-08-18 18:40:07 +0300

    stage3: ROS: upgrade to Kinetic

--- stage3_emlid/07-ros/00-run.sh
+++ stage3_emlid/07-ros/00-run.sh
@@ -3,6 +3,12 @@
 on_chroot apt-key add - < files/ros.key
 
 on_chroot << EOF
-echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list
+echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list
 apt-get update
 EOF
+
+cp -r files ${ROOTFS_DIR}/files
+on_chroot << EOF
+dpkg -i files/*.deb
+EOF
+rm -rf ${ROOTFS_DIR}/files
