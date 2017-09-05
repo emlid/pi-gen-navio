d2e7f6c121cdc48354cb0f3061e09511913c2d47
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-09-05 19:41:27 +0300

    stage3: python: add emlidtool debian package

--- stage3_emlid/09-python/01-run.sh
+++ stage3_emlid/09-python/01-run.sh
@@ -10,3 +10,10 @@ EOF
 
 rm -rf ${ROOTFS_DIR}/home/pi/requirements*.txt
 rm -rf ${ROOTFS_DIR}/home/pi/emlid*
+
+mkdir -p ${ROOTFS_DIR}/files
+install -m 644 files/*.deb ${ROOTFS_DIR}/files
+on_chroot << EOF
+dpkg -i files/*.deb
+EOF
+rm -rf ${ROOTFS_DIR}/files
