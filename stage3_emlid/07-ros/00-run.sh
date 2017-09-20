0b827810e9ca145a841b55479eb4b7e142837ea1
Georgii Staroselskii <georgii.staroselskii@emlid.com>
2017-09-20 14:43:32 +0300

    stage3: ros: move hacks to apt-get

--- stage3_emlid/07-ros/00-run.sh
+++ stage3_emlid/07-ros/00-run.sh
@@ -6,9 +6,3 @@ on_chroot << EOF
 echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list
 apt-get update
 EOF
-
-cp -r files ${ROOTFS_DIR}/files
-on_chroot << EOF
-dpkg -i files/*.deb
-EOF
-rm -rf ${ROOTFS_DIR}/files
