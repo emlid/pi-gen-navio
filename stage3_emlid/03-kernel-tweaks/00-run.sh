8ec13fa53f2c799c4fa2b99dd77d6514ce502f8b
Alexey Bulatov <alexey.bulatov@emlid.com>
2018-05-11 16:52:02 +0300

    stage3: kernel: install kernel from apt
    Substitute Raspbian's packages for kernel and headers with our.
    Removed old packages for it. They are obsolete.

--- stage3_emlid/03-kernel-tweaks/00-run.sh
+++ stage3_emlid/03-kernel-tweaks/00-run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash -ex
 
 install -m 644 files/config.txt ${ROOTFS_DIR}/boot/
-
-on_chroot << EOF
-apt-get purge libraspberrypi-dev raspberrypi-bootloader raspberrypi-kernel -y
-apt-get autoremove -y
-PACKAGE_NAME=$(debconf-get-selections | grep 'linux-image-' | cut -f1 | head -1)
-PACKAGE_VERSION=$(echo $PACKAGE | cut -d- -f3-)
-echo "$PACKAGE_NAME $PACKAGE_NAME/preinst/overwriting-modules-$PACKAGE_VERSION boolean false" | debconf-set-selections
-EOF
