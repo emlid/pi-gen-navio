80902fbed1fac5de7345fee27f9d67309922676e
Alexey Bulatov <alexey.bulatov@emlid.com>
2018-05-17 13:00:56 +0300

    stage3: apt: pin kernel and emlid repo
    Make high priority for packages from emlid's server.
    Ban raspberrypi-kernel and raspberrypi-kernel-headers from
    archive.raspberrypi.org. Install only versions from emlid.

--- stage3_emlid/02-apt-tweaks/00-run.sh
+++ stage3_emlid/02-apt-tweaks/00-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 install -m 644 files/emlid.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
+install -m 644 files/emlid ${ROOTFS_DIR}/etc/apt/preferences.d/
+install -m 644 files/kernel ${ROOTFS_DIR}/etc/apt/preferences.d/
 
 on_chroot apt-key add - < files/emlid.gpg.key
 on_chroot apt-key add - < files/emlid-builder.gpg.key
