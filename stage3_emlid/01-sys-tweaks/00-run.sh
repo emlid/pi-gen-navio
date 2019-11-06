784ad482bab26c82606cc18486bb1a0c1da1373a
Alexey Bulatov <alexey.bulatov@emlid.com>
2019-11-06 17:14:21 +0300

    stage3: add system tweaks
    - patch hosts file (change from raspberrypi to navio)
    - default wpa_supplicant file with link to boot dir
    - i2c-dev module
    - custom interfaces file
    - /etc/environment file with en_US.UTF-8 locale
    - cmdline with disabled UART console

---
+++ stage3_emlid/01-sys-tweaks/00-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+install -m 644 files/wpa_supplicant.conf ${ROOTFS_DIR}/boot
+install -m 644 files/modules ${ROOTFS_DIR}/etc/modules
+install -m 644 files/cmdline.txt ${ROOTFS_DIR}/boot
+install -m 644 files/environment ${ROOTFS_DIR}/etc
+install -m 644 /dev/null ${ROOTFS_DIR}/boot/ssh
+install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network
+
+ln -sf /boot/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
+
+on_chroot << EOF
+rm -f /etc/motd
+systemctl disable hciuart
+dpkg-divert --add --local /etc/wpa_supplicant/wpa_supplicant.conf
+EOF
