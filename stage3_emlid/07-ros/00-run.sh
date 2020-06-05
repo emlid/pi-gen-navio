56864e0125283d23d47a63521ec0809c505bc85a
acxz <17132214+acxz@users.noreply.github.com>
2020-06-05 00:12:52 -0400

    update to ros-noetic

--- stage3_emlid/07-ros/00-run.sh
+++ stage3_emlid/07-ros/00-run.sh
@@ -3,6 +3,6 @@
 on_chroot apt-key add - < files/ros.key
 
 on_chroot << EOF
-echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list
+echo "deb http://packages.ros.org/ros/ubuntu focal main" > /etc/apt/sources.list.d/ros-latest.list
 apt-get update
 EOF
