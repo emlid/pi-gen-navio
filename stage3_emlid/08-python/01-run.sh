70dcdfb0887433da1115abc192aeaa71fe27103e
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2020-11-23 21:05:50 +0100

    Fix issue with default user name not 'pi'

--- stage3_emlid/08-python/01-run.sh
+++ stage3_emlid/08-python/01-run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -ex
 
-cp -f files/requirements3.txt ${ROOTFS_DIR}/home/pi
-cp -f files/requirements2.txt ${ROOTFS_DIR}/home/pi
+cp -f files/requirements3.txt ${ROOTFS_DIR}/home/${FIRST_USER_NAME}
+cp -f files/requirements2.txt ${ROOTFS_DIR}/home/${FIRST_USER_NAME}
 
 on_chroot << EOF
-pip3 install -r /home/pi/requirements3.txt
-pip2 install -r /home/pi/requirements2.txt
+pip3 install -r /home/${FIRST_USER_NAME}/requirements3.txt
+pip2 install -r /home/${FIRST_USER_NAME}/requirements2.txt
 EOF
 
-rm -rf ${ROOTFS_DIR}/home/pi/requirements*.txt
-rm -rf ${ROOTFS_DIR}/home/pi/emlid*
+rm -rf ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/requirements*.txt
+rm -rf ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/emlid*
